59c709616b282f23b6151c6c3084f4f711582ecf
daman-talkshoplive <112784890+daman-talkshoplive@users.noreply.github.com>
2023-11-08 10:09:43 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "JitsiWebRTC",
+    name: "TSLWebRTC",
     platforms: [.iOS(.v12)],
     products: [
         .library(
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/jitsi/webrtc/releases/download/v118.0.0/WebRTC.xcframework.zip",
+            url: "https://github.com/daman-talkshoplive/webrtc/releases/download/v118.0.3/WebRTC.xcframework.zip",
             checksum: "e01960ca52aca9acff3ae712391a5eee4393fd5a32accab0c3801f330615ea1b"
         ),
     ]
